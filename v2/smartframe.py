ca767892133e3f996be625d230170a7aba319741
summerliehu <1542659276@qq.com>
2018-01-14 16:58:20 +0800

    complete v2

--- v2/smartframe.py
+++ v2/smartframe.py
@@ -5,54 +5,62 @@ from werkzeug.wrappers import Response, Request
 from werkzeug.serving import run_simple
 from werkzeug.routing import Map, Rule, NotFound, RequestRedirect
 
-url_map = Map([
-    Rule('/', endpoint='index')
-])
 
-class Application(object):
 
+class Route:
     def __init__(self):
-        self.endpoint_dict = {
-        #'index':index
-        #'test':test,
-        }
+        self.url_map = Map([])
+        self.endpoint_dict = {}
 
-    def route(self, rules, endpoint):
+    def __call__(self, rules, endpoint_value=0):
         def wrapper(func):
-            url_map.add(Rule(rules, endpoint=endpoint))
+            endpoint = endpoint_value
+            if not endpoint:
+                endpoint = func.__name__
+            self.url_map.add(Rule(rules, endpoint=endpoint))
             self.endpoint_dict[endpoint] = func
             return func
         return wrapper
 
+class Application(object):
+
+    def __init__(self):
+        self.route = Route()
+
     def __call__(self, environ, start_response):
         return self.application(environ, start_response)
 
-    def application(self,environ, start_response):
-        urls = url_map.bind_to_environ(environ)
-        #url_map.add(Rule('/test/', endpoint='test'))
-        environ = environ
+    @Request.application
+    def application(self,request):
+        urls = self.bind_to_environ(request.environ)
         try:
             endpoint, args = urls.match()
-        except HTTPException, e:
-            return e(environ, start_response)
+        except Exception, e:
+            return Response(str(e))
+        return self.route.endpoint_dict[endpoint](request)
 
-        return self.endpoint_dict[endpoint](start_response, **args)
+    def bind_to_environ(self, env):
+        return self.route.url_map.bind_to_environ(env)
 
 app = Application()
 
 @app.route('/', 'index')
-def index(start_response, **args):
-    start_response('200 OK', [('Content-Type','text/plain')])
-    return "Welcome to Index!"
+def index(request):
+    urls = app.bind_to_environ(request.environ)
+    endpoint = urls.match()[0]
+    return Response("Welcome to %s!" % endpoint)
 
-@app.route('/id/<int:id>', 'id')
-def id(start_response, **args):
-    start_response('200 OK', [('Content-Type','text/plain')])
-    #return ["args: %s" % args["id"]]
-    return "id is: %s" % args["id"]
+@app.route('/id/<int:id>')
+def id(request):
+    urls = app.bind_to_environ(request.environ)
+    values = urls.match()[1]
+    return Response("id is: %s" % values["id"])
 
-
-
+@app.route('/current_url/', 'current_url')
+def current_url(request):
+    urls = app.bind_to_environ(request.environ)
+    url = urls.build("current_url")
+    return Response("current url is %s" % url)
 
 
 run_simple('localhost', 8000, app, use_reloader = True)
